d6e84a31da535951a9fde58f08840796d1963594
tonyjiangtb <tonyjiangtb@gmail.com>
2017-04-13 22:51:31 +0800

    create

---
+++ rrr/src/main/java/ttt/rrr/App.java
@@ -0,0 +1,14 @@
+package ttt.rrr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//change test
+        System.out.println( "Hello World!" );
+    }
+}
